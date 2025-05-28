914075ab004c9cf01d00868c25fc13437c83a10e
Pietro Albini <pietro@pietroalbini.org>
2025-05-28 12:35:09 +0200

    register runners with jitconfig and pass it with systemd creds

--- executor/run.py
+++ executor/run.py
@@ -4,34 +4,7 @@ from executor.github import GitHub
 from executor.qemu import VM
 import argparse
 import json
-import re
 import signal
-import sys
-
-
-class ConfigPreprocessor:
-    # This regex matches: ${{ FUNCTION:ARGS }}
-    _VARIABLE_RE = re.compile(r"^\${{ *(?P<function>[a-zA-Z0-9_-]+):(?P<args>[^}]+)}}$")
-
-    def __init__(self, config, gh: GitHub):
-        self._config = config
-        self._gh = gh
-
-    def process(self):
-        for key, value in self._config.items():
-            matches = self._VARIABLE_RE.match(value)
-            if matches is None:
-                continue
-
-            function = matches.group("function").strip()
-            args = matches.group("args").strip()
-
-            if function == "gha-install-token":
-                self._config[key] = self._gh.fetch_registration_token()
-            else:
-                raise ValueError(f"unknown preprocessor function: {function}")
-
-        return self._config
 
 
 signal_vms = []
@@ -45,27 +18,13 @@ def sigusr1_received(sig, frame):
 def run(cli):
     signal.signal(signal.SIGUSR1, sigusr1_received)
 
+    with open(cli.instance_spec) as f:
+        instance = json.load(f)
+
     gh = GitHub(cli)
+    runner = gh.create_runner(cli, instance)
 
-    with open("instances.json") as f:
-        instances = json.load(f)
-
-    instance = None
-    for candidate in instances:
-        if candidate["name"] == cli.instance_name:
-            instance = candidate
-            break
-    else:
-        print(f"error: instance not found: {cli.instance_name}", file=sys.stderr)
-        exit(1)
-
-    config = ConfigPreprocessor(instance["config"], gh)
-    env = {
-        "name": instance["name"],
-        "config": config.process(),
-    }
-
-    vm = VM(instance, env)
+    vm = VM(cli, instance, runner)
     signal_vms.append(vm)
 
     vm.run(gh)
@@ -74,7 +33,7 @@ def run(cli):
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("instance_name")
+    parser.add_argument("instance_spec")
 
     parser.add_argument(
         "--github-client-id",
@@ -89,6 +48,13 @@ def main():
     parser.add_argument(
         "--github-org",
         help="GitHub org to register the runner into",
+        required=True,
+    )
+
+    parser.add_argument(
+        "--runner-group-id",
+        help="ID of the runner group to register the runner into",
+        type=int,
         required=True,
     )
 
