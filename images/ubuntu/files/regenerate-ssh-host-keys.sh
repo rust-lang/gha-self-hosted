2e7f7b060132f5b406dcbafac87ecfdbf8fd5f2f
Pietro Albini <pietro@pietroalbini.org>
2025-06-05 11:06:26 +0200

    inline regenerate-ssh-host-keys script into systemd unit

--- images/ubuntu/files/regenerate-ssh-host-keys.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -euo pipefail
-IFS=$'\n\t'
-
-export DEBIAN_FRONTEND=noninteractive
-
-# Regenerate the SSH host keys, which were deleted during VM creation.
-dpkg-reconfigure openssh-server
